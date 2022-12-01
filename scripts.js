c286da9138175b5cf768aff92d296361908d1ebb
Joshua Church <joshchurch95@gmail.com>
2022-12-01 16:12:25 -0500

    Add logic and styles for grid creation

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+const squaresContainer = document.querySelector('#squares-container');
+const GRID_SIZE = 16;
+
+for (let i = 0; i < GRID_SIZE; i++)
+{
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j = 0; j < GRID_SIZE; j++)
+    {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        row.appendChild(square);
+    }
+    squaresContainer.appendChild(row);
+}
